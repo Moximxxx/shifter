699dbd77de733b9d3b94b182d3fc2c6430d14b21
moximxxx <921291083@qq.com>
2026-06-15 21:24:25 +0800

    release: v1.0.3

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ set -e
 
 # --- Configuration ---
 REPO="Moximxxx/shifter"
-DEFAULT_VERSION="1.0.2"
+DEFAULT_VERSION="1.0.3"
 VERSION="${VERSION:-$DEFAULT_VERSION}"
 INSTALL_DIR="${INSTALL_DIR:-$HOME/.local/bin}"
 BINARY="shifter"
